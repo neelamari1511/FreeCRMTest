5b5fd865878a537591e03f09f7818c845ca959d7
neelamari <neelamari1511@gmail.com>
2018-12-26 20:47:57 +0530

    modified

--- src/test/java/com/crm/qa/testcases/ContactsPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -1,7 +1,6 @@
 package com.crm.qa.testcases;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
